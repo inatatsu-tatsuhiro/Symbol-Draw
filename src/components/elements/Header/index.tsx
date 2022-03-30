d64086f3fccc88ac24eec64e026d7851505a6a8d
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2022-03-30 19:01:35 +0900

    :sparkles: UI更新

--- src/components/elements/Header/index.tsx
+++ src/components/elements/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import styled from '@emotion/styled'
 
-import { AppBar, Box, Toolbar } from '@mui/material'
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
 import Color from '../../../utils/Color'
 import Button from '../Button'
 import TextField from '../TextField'
@@ -12,6 +12,7 @@ import MButton from '@mui/material/Button'
 import { useI18n } from '../../../utils/useI18n'
 import { usePrikey } from '../../../utils/PrikeyContext'
 import { Account, Address, NetworkType } from 'symbol-sdk'
+import { BsDownload } from 'react-icons/bs'
 
 export interface Props {
   navi: (path: string) => void
@@ -19,19 +20,16 @@ export interface Props {
 
 const Component: React.VFC<Props> = ({ navi }) => {
   const { lang, selectEn, selectJa } = useI18n()
-  const cx = usePrikey()
 
-  const getAddr = () => {
-    if (cx === undefined) return ''
-    try {
-      const acc = Account.createFromPrivateKey(cx.prikey, NetworkType.TEST_NET)
-      if (acc.address.plain() === 'TDEC5VUUAUYHKI2Y45WBDMGODAS42P3PPCTMGUY')
-        return 'GEST'
-      return acc.address.plain()
-    } catch {
-      return '404'
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const submit = () => {
+    if (inputRef === null || inputRef.current === null) {
+      return
     }
+    navi(`/canvas/${inputRef.current.value}`)
   }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <SAppBar position="static" color="default">
@@ -41,7 +39,14 @@ const Component: React.VFC<Props> = ({ navi }) => {
             <div>Draw</div>
           </Logo>
           <Box sx={{ flexGrow: 1 }} />
-          {/* <Space margin="4px">{getAddr()}</Space> */}
+          <Search>
+            <TextField inputRef={inputRef} text="TxHash" />
+            <Space margin="8px">
+              <IconButton component="span" onClick={submit}>
+                <BsDownload />
+              </IconButton>
+            </Space>
+          </Search>
           <Space margin="0px 40px">
             {lang === 'ja' ? (
               <MButton onClick={selectEn}>{lang}</MButton>
@@ -49,14 +54,6 @@ const Component: React.VFC<Props> = ({ navi }) => {
               <MButton onClick={selectJa}>{lang}</MButton>
             )}
           </Space>
-          {/* <Space margin="4px">
-            <Button
-              text={'CONNECT'}
-              onClick={() => {
-                navi('/connect')
-              }}
-            />
-          </Space> */}
         </Toolbar>
       </SAppBar>
     </Box>
@@ -80,3 +77,8 @@ const Logo = styled('div')`
   flex-direction: column;
   align-items: center;
 `
+const Search = styled('div')({
+  width: '600px',
+  display: 'flex',
+  justifyContent: 'space-between',
+})
--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 
-const Home = React.lazy(() => import('../features/home'))
 const Canvas = React.lazy(() => import('../features/draw'))
 const Audit = React.lazy(() => import('../features/audit'))
 const Connect = React.lazy(() => import('../features/connect'))
@@ -10,9 +9,10 @@ const Connect = React.lazy(() => import('../features/connect'))
 const Router: React.VFC = () => {
   return (
     <React.Suspense fallback={<div>LOADING...</div>}>
+      {/* <BrowserRouter> */}
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<Layout page={<Home />} />} />
+          <Route path="/" element={<Navigate to="/canvas" replace />} />
           <Route path="/canvas/:hash" element={<Layout page={<Canvas />} />} />
           <Route path="/canvas" element={<Layout page={<Canvas />} />} />
           <Route path="/audit" element={<Layout page={<Audit />} />} />
